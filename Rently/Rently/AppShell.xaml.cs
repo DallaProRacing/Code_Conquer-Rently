8ee83f931cc7d6d9b6c0a940c75da46a32ed268b
henri-grz <164960212+henri-grz@users.noreply.github.com>
2025-09-17 20:46:43 -0300

    Alguns ajustes

--- Rently/Rently/AppShell.xaml.cs
+++ Rently/Rently/AppShell.xaml.cs
@@ -5,6 +5,7 @@
         public AppShell()
         {
             InitializeComponent();
+            
             Routing.RegisterRoute("Cadastro", typeof(Cadastro));
             Routing.RegisterRoute("Senha", typeof(Senha));
             Routing.RegisterRoute("Dashboard", typeof(Dashboard));
