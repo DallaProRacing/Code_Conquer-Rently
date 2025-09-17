18e5aff02223fc299bfa7b79da31c6cb1797e447
henri-grz <164960212+henri-grz@users.noreply.github.com>
2025-09-17 20:52:37 -0300

    Ajuste de cores e rotas

--- Rently/Rently/AppShell.xaml.cs
+++ Rently/Rently/AppShell.xaml.cs
@@ -11,6 +11,7 @@
             Routing.RegisterRoute("Dashboard", typeof(Dashboard));
             Routing.RegisterRoute("Perfil", typeof(Perfil));
             Routing.RegisterRoute("Produtos", typeof(Produtos));
+            Routing.RegisterRoute("Anuncios", typeof(Anuncios));
         }
     }
 }
